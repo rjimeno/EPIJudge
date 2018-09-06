329eca3a3a3695fc86b5f2f66aae2cfd7e02c76e
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-09-06 01:24:36 -0400

    4.1 Computing the parity of a word: Page 27.

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
 
-def parity(x):
-    # TODO - you fill in here.
-    return 0
+def parity(natural):
+    if natural == 0:
+        parity = 0
+    else:
+        seen_ones = 0
+        while natural > 0:
+            seen_ones += natural % 2  # 1 when natural is odd
+            natural = natural >> 1
+        parity = seen_ones %2
+    return parity
 
 
 if __name__ == '__main__':
