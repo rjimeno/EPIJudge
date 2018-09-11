5f124aa3eb76ea66f66144242cf893709b8c8dc9
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-09-11 19:02:40 -0400

    Seemingly hangs for large inputs (over 9999 prices).

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,9 +1,33 @@
 from test_framework import generic_test
 
+# Feels like it hangs when trying to process a list of 1*10**5 (or so)prices
+def buy_and_sell_stock_once_rjimeno_brute_force(prices):
+#    print(f" prices == {prices} ", end='')
+    if len(prices) < 2:
+        return 0.0
+    best_i = 0
+    best_j = 1
+    best_gain = 0
+    i = 0
+    while i < len(prices) - 1:
+        j = i + 1
+        while j < len(prices):
+            this_gain = prices[j] - prices[i]
+            if  this_gain > best_gain:
+                best_gain = this_gain
+                best_i = i
+                best_j = j
+            j += 1
+        i += 1
+    return best_gain
 
-def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
+
+def buy_and_sell_stock_once_book(prices):
+    # code goes here
     return 0.0
+
+
+buy_and_sell_stock_once = buy_and_sell_stock_once_rjimeno_brute_force
 
 
 if __name__ == '__main__':
