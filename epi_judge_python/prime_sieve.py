4f59b2ab344a808d09fd204608d7c085e8cf6995
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-09-14 17:01:55 -0400

    I was interested in the book approach only; will try personal approach later.

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -3,8 +3,16 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n):
-    # TODO - you fill in here.
-    return []
+    primes = []
+    # is_prime[p] represents if p is prime or not. Initially, set each to
+    # true, except 0 and 1. Then use sieving to eliminate non primes.
+    is_prime = [False, False] + [True] * (n - 1)
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            primes.append(p)
+            for i in range(p, n + 1, p):
+                is_prime[i] = False
+    return primes
 
 
 if __name__ == '__main__':
