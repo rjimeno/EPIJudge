4db6c91f27a35dfa2d79792adfb1d8dcec5f12d3
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-09-14 22:00:24 -0400

    My solution uses O(2n) space while the book's is O(n).

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -1,10 +1,35 @@
 from test_framework import generic_test
 
 
-def apply_permutation(perm, A):
-    # TODO - you fill in here.
-    return
+def apply_permutation_rjimeno(perm, A):
+    tmp = [0] * len(A)
+    for i in range(len(perm)):
+        tmp[perm[i]] = A[i]
+    A[:] = tmp
+    return A
 
+def apply_permutation_book(perm, A):
+    def cyclic_permutation(start, perm, A):
+        i, temp = start, A[start]
+        while True:
+            next_i = perm[i]
+            next_temp = A[next_i]
+            A[next_i] = temp
+            i, temp = next_i, next_temp
+            if i == start:
+                break
+    for i in range(len(A)):
+        # Traverses the cycle to see if i is the minumum element.
+        j = perm[i]
+        while j != i:
+            if j < i:
+                break
+            j = perm[j]
+        else:
+            cyclic_permutation(i, perm, A)
+
+
+apply_permutation = apply_permutation_rjimeno
 
 def apply_permutation_wrapper(perm, A):
     apply_permutation(perm, A)
