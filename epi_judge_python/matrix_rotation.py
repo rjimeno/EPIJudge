68bc82cfe35e97320d1ff72157c6e65d487c7b86
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-09-21 22:41:36 -0400

    Leveraged the NumPy (numpy) library for this solution.

--- epi_judge_python/matrix_rotation.py
+++ epi_judge_python/matrix_rotation.py
@@ -1,10 +1,24 @@
 from test_framework import generic_test
+import numpy as np
+
+
+def rotate_matrix_rjimeno(square_matrix):
+    if [] == square_matrix:
+        return []
+    m = np.array(square_matrix, int)
+    m = np.rot90(m)
+    m = np.rot90(m)
+    m = np.rot90(m)
+    m = m.tolist()
+    square_matrix[:] = m[:]
+    return
 
 
 def rotate_matrix(square_matrix):
     # TODO - you fill in here.
     return
 
+rotate_matrix = rotate_matrix_rjimeno
 
 def rotate_matrix_wrapper(square_matrix):
     rotate_matrix(square_matrix)
