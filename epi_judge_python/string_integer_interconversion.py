595a143b37282ca9855e072947236f7af34c11e8
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-23 01:52:47 -0400

    Uses a couple of lookup tables and iterations to make the conversion.

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -2,14 +2,36 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
-def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+def int_to_string_rjimeno(x):
+    if x < 0:
+        sign = '-'
+        x = -x
+    elif 0 == x:
+        return '0'
+    else:
+        sign = ''
+    i_to_s = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+    s = ''
+    while 0 < x:
+        s = i_to_s[x % 10] + s
+        x //= 10
+    s = sign + s
+    return s
 
 
-def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+def string_to_int_rjimeno(s):
+    if '-' == s[0]:
+        sign = -1
+        s = s[1:]
+    else:
+        sign = 1
+    rs = s[::-1]
+    s_to_i = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    tmp = 0
+    for i in range(len(rs)):
+        tmp += s_to_i[rs[i]] * 10 ** i
+    tmp *= sign
+    return tmp
 
 
 def wrapper(x, s):
@@ -17,6 +39,10 @@ def wrapper(x, s):
         raise TestFailure("Int to string conversion failed")
     if string_to_int(s) != x:
         raise TestFailure("String to int conversion failed")
+
+
+int_to_string = int_to_string_rjimeno
+string_to_int = string_to_int_rjimeno
 
 
 if __name__ == '__main__':
