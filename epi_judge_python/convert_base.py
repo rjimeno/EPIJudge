8bb966663b0a2b8d514af4e461d9b0f76aebe0bd
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-09-23 23:15:09 -0400

    My solution would have been much lengthier and convoluted.

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,24 @@
+import functools
+import string
+# Two lines above from EPIJudge solutions.
 from test_framework import generic_test
 
 
-def convert_base(num_as_string, b1, b2):
-    # TODO - you fill in here.
-    return ''
+def convert_base_book(num_as_string, b1, b2):
+    def construct_from_base(num_as_int, base):
+        return ('' if num_as_int == 0 else
+                construct_from_base(num_as_int // base, base) +
+                string.hexdigits[num_as_int % base].upper())
+
+    is_negative = num_as_string[0] == '-'
+    num_as_int = functools.reduce(
+        lambda x, c: x * b1 + string.hexdigits.index(c.lower()),
+        num_as_string[is_negative:], 0)
+    return ('-' if is_negative else '') + ('0' if num_as_int == 0 else
+                                           construct_from_base(num_as_int, b2))
+
+
+convert_base = convert_base_book
 
 
 if __name__ == '__main__':
