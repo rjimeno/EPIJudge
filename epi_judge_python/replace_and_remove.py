dad9e1c1d4a4ec965c267aeaf63302a9d9b2ac85
Roberto Jimeno <roberto.jimeno@gmail.com>
2019-02-24 21:34:26 -0500

    My solution trades space for time compared to the book's solution.

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -4,10 +4,50 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
-def replace_and_remove(size, s):
-    # TODO - you fill in here.
-    return 0
+def replace_and_remove_rjimeno(size, s):
+    # 'a' -> 'dd'
+    # 'b' -> ''
+    # This solution has uses time O(n) & space O(2n).
+    l = []
+    for c in s[:size]:
+        if c == 'a':
+            l.append('d')
+            l.append('d')
+        elif c == 'b':
+            pass
+        else:
+            l.append(c)
+    t = ''.join(l)
+    for i in range(0, len(t)):
+        s[i] = t[i]
+    return len(t)
 
+def replace_and_remove_book(size, s):
+
+    # Forward iteration: remove 'b's and count the number of 'a's.
+    write_idx, a_count = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+
+    # Backward iteration: replace 'a's with 'dd's starting from the end.
+    cur_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+    while cur_idx >= 0:
+        if s[cur_idx] == 'a':
+            s[write_idx - 1:write_idx + 1] = 'dd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[cur_idx]
+            write_idx -= 1
+        cur_idx -= 1
+    return final_size
+
+replace_and_remove = replace_and_remove_rjimeno
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
